e3a95e2ba9a7eb76c743e78a5f6084a958be7911
jjaferson <jmonteir@redhat.com>
2020-04-06 15:47:20 +0100

    add maven web project structure

---
+++ target/classes/authenticator/DelayAuthentication.java
@@ -0,0 +1,41 @@
+package authenticator;
+
+import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+
+public class DelayAuthentication implements Authenticator {
+
+	public void close() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void authenticate(AuthenticationFlowContext context) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void action(AuthenticationFlowContext context) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean requiresUser() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean configuredFor(KeycloakSession session, RealmModel realm, UserModel user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void setRequiredActions(KeycloakSession session, RealmModel realm, UserModel user) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
